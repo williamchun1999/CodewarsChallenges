f0657da1a4c23a3bfb8e04d5c73b877c4d5f629a
williamchun1999 <williamchun1999@gmail.com>
2022-04-20 16:10:18 -0700

    Added one 7KYU Solution

--- 7KYU.js
+++ 7KYU.js
@@ -21,4 +21,45 @@ function unusedDigits(...nums) {
     }
     return result;
   }
-// Too many values.. Definitely should refactor into a filter method instead.
+/* Comments
+
+Too many values.. Definitely should refactor into a filter method instead.
+
+*/
+
+
+
+/* April 20, 2022
+Write a generic function chainer
+Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for ruby).
+
+The input for each function is the output of the previous function (except the first function, which takes the starting value as it's input). Return the final value after execution is complete.
+
+Example:
+function add(num) {
+  return num + 1
+}
+
+function mult(num) {
+  return num * 30
+}
+
+chain(2, [add, mult]);
+// returns 90;
+*/
+
+// MY SOLUTION
+function chain(input, fs) {
+  for (let i = 0; i < fs.length; i++){
+    input = fs[i](input);
+  }
+  return input;
+}
+/* Comments
+
+  Bit hard to read. Using a reduce method would be more readable, letting us know it's combining results of previous functions to subsequent functions.
+
+*/
+
+
+
