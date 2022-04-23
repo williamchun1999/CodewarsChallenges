94599f95b80f701cfbc7f472b7bfb6d0cb410cfe
williamchun1999 <williamchun1999@gmail.com>
2022-04-22 23:24:44 -0700

    added 6KYU linked list solution

--- 6KYU.js
+++ 6KYU.js
@@ -41,3 +41,48 @@ function Node(data) {
 /* Comments 
   Could have combined both edge cases into a single if statement.
 */
+
+/* 
+April 22 2022
+Linked Lists - Insert Sort
+
+Write an InsertSort() function which rearranges nodes in a linked list so they are sorted in increasing order. You can use the SortedInsert() function that you created in the "Linked Lists - Sorted Insert" kata below. The InsertSort() function takes the head of a linked list as an argument and must return the head of the linked list.
+
+var list = 4 -> 3 -> 1 -> 2 -> null
+insertSort(list) === 1 -> 2 -> 3 -> 4 -> null
+If the passed in head node is null or a single node, return null or the single node, respectively. You can assume that the head node will always be either null, a single node, or a linked list consisting of multiple nodes.
+
+The push(), buildOneTwoThree(), and sortedInsert() functions need not be redefined.
+
+*/
+
+//MY SOLUTION
+
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function insertSort(head) {
+  // covers for a head of null
+  if (!head){
+    return null;
+  }
+  // creates a reference to sort incoming heads into the list and where to put it
+  let listReference = new Node(head.data);
+  // since we make the reference start with the initial head value, we start the rest of the head with head.next;
+  //need to cover for head where next = null;
+  head = head.next;
+  // While there is a head, we input the head to our premade sortedInsert function from the question ytd into our reference
+  //until head = null;
+  while (head){
+     listReference = sortedInsert(listReference, head.data)
+    head = head.next;
+  }
+  return listReference;
+  
+}
+
+/* Comments
+  writing out pseudocode made me solve five times faster compared to the previous linked list question.
+*/
